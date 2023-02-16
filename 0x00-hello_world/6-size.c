e79e1ecc4ee74355d6f7d62dd220643a0826a163
PeshO2K <patienceotuke@gmail.com>
2023-02-16 11:34:58 -0800

    added newline plus long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Size of a char: %d bytes(s)",sizeof(char));
-	printf("Size of a int: %d bytes(s)",sizeof(int));
-	printf("Size of a long long int: %d byte(s)",sizeof(long long));
-	printf("Size of a float: %d byte(s)",sizeof(float));
+	printf("Size of a char: %d byte(s)\n",sizeof(char));
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	printf("Size of a long int: %d byte(s)\n",sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(long long));
+	printf("Size of a float: %d byte(s)\n",sizeof(float));
 
 	return(0);
 }
