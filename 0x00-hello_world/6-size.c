fb704f8e5e9b279ab814ec8efdf43248c7224254
PeshO2K <patienceotuke@gmail.com>
2023-02-16 11:30:44 -0800

    Size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)",sizeof(char));
+	printf("Size of a int: %d bytes(s)",sizeof(int));
+	printf("Size of a long long int: %d byte(s)",sizeof(long long));
+	printf("Size of a float: %d byte(s)",sizeof(float));
+
+	return(0);
+}
