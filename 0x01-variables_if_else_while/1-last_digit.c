f46e10ae83884aef30c113680ad5904bc3c06ab9
PeshO2K <patienceotuke@gmail.com>
2023-02-17 01:30:49 -0800

    last digit added closing bracket

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,14 +17,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	if (l > 5){
+	if (l > 5)
+	{
                strcpy(str,"greater than 5");
         }
-        else if (n == 0) {
+        else if (n == 0)
+	{
                strcpy(str,"0");
         }
-        else {
+        else 
+	{
                strcpy(str,"less than 6 and not 0");
+	}
 	/* your code goes there */
 	printf("Last digit of %d is %d and is %s\n",n,l,str);
 
