e79a11520d53a68749ce88a593d671065a91b7de
PeshO2K <patienceotuke@gmail.com>
2023-02-17 01:27:58 -0800

    last digit str declaration correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
  * main - Entry point
@@ -11,19 +12,19 @@ int main(void)
 {
 	int n;
 	int l;
-	//char str [];
+	char str[] = "";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 	if (l > 5){
-               char str[] = "greater than 5";
+               strcpy(str,"greater than 5");
         }
         else if (n == 0) {
-               char  str[] = "0";
+               strcpy(str,"0");
         }
         else {
-               char str[] = "less than 6 and not 0";
+               strcpy(str,"less than 6 and not 0");
 	/* your code goes there */
 	printf("Last digit of %d is %d and is %s\n",n,l,str);
 
