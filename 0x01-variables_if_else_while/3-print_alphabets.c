6e187611f310e35dcd797430dbb373d1213c084b
PeshO2K <patienceotuke@gmail.com>
2023-02-17 02:37:21 -0800

    upper and Lowercase Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-        int a = 97;
+	int a = 97;
 	int b = 65;
 
-        while (a < 123)
-        {
-                putchar(a);
-                a++;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
 	}
 	while (b < 91)
-        {
-                putchar(b);
-                b++;
-        }
+	{
+		putchar(b);
+		b++;
+	}
 	putchar('\n');
-
-        return (0);
+	
+	return (0);
 
 }
