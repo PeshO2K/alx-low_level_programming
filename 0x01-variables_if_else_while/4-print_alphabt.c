4d953b3bab3966308da55ff6ca120f6ac58625c3
PeshO2K <patienceotuke@gmail.com>
2023-02-17 03:17:34 -0800

    alpha soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,10 @@ int main(void)
 {
         int a;
         
-        for (a = 97; a < 123 && a != 101 && a != 113, a++)
+        for (a = 97; a < 123 && a != 101 && a != 113; a++)
         {
                 putchar(a);
-                a++;
-        }
+	}
 
         putchar('\n');
         return (0);
