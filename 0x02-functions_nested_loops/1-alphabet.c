3e7d8151ee3aeb2a73139cec101461430446350e
PeshO2K <patienceotuke@gmail.com>
2023-02-23 03:12:22 -0800

    betty correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 #include "main.h"
 /**
  *  print_alphabet- prints alpahabet in lower case
- * 
+ *
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 	int a = 97;
+	
 	while (a <= 122)
 	{
 		putchar(a);
