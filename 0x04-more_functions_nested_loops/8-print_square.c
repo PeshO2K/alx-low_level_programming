87e2133af261ba9a610cf31e4fbcd6e08bb53cda
PeshO2K <patienceotuke@gmail.com>
2023-02-23 14:18:55 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints square
+ * @size - size
+ * Return: void
+ */
+void print_square(int size)
+{
+	int r, c;
+
+	if (size > 0)
+	{
+		for (r = 0; r < size; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		
+	}
+	_putchar('\n');
+}
