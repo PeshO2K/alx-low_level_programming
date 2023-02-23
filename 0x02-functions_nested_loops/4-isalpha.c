a658b46f7563c19ac5c3e36047faf18260ce8134
PeshO2K <patienceotuke@gmail.com>
2023-02-23 04:14:44 -0800

    Alphabetical Character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,8 @@
  * _isalpha - checks for alphabetic character
  *
  * @c: Character being checked
- * Return: 1 if alpabetic charcater, 0 otherwise */
+ * Return: 1 if alpabetic charcater, 0 otherwise
+ */
 int _isalpha(int c)
 {
 	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
