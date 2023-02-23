6c4e5e0a0c41936dcbffcf3181ec0aeb599ca483
PeshO2K <patienceotuke@gmail.com>
2023-02-23 03:22:46 -0800

    Alphabet game X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always 0
+ */
+void print_alpahbet_x10(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		int a = 97;
+
+		while  (a <= 122)
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+	}
+}
