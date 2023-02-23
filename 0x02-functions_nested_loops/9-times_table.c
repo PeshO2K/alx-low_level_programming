b44c8dcc348fb6efcd4a89d40a30ecca5ce9897d
PeshO2K <patienceotuke@gmail.com>
2023-02-23 07:02:52 -0800

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - multiplication table of 9
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int r, c, p;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <=9; c++)
+		{
+			p = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n')
+	}
+}
