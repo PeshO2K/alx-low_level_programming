4a96008f959c9605a7d34909e4b737085a538162
PeshO2K <patienceotuke@gmail.com>
2023-02-23 04:36:38 -0800

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+/**
+ * _abs - computes absolute value of integer
+ *
+ * @n: Integer
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
