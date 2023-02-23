eeeeb21d78d183356bd88f3c53dabe72b9632b7a
PeshO2K <patienceotuke@gmail.com>
2023-02-23 06:43:09 -0800

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints all minutes in 24hours
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h + '0');
+			_putchar(':');
+			_putchar(m + '0');
+			_putchar('\n');
+		}
+	}
+}
