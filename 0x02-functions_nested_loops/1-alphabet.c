90feeef176496aa917a1fd07dfeac38baa8ab24d
PeshO2K <patienceotuke@gmail.com>
2023-02-23 03:07:10 -0800

    added a++;x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,26 +5,13 @@
  * Write a function that prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 
  */
-/*void print_alphabet(void)
-{
-	int a = 97;
-	while(a <=122)
-	{
-		putchar(a);
-	}
-	putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}*/
 void print_alphabet(void)
 {
-	int a =97;
+	int a = 97;
 	while(a <= 122)
 	{
 		putchar(a);
+		a++;
 	}
 	putchar('\n');
 }
