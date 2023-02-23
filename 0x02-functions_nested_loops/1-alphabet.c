b3f7749604e33d036366b08b86c93e3e0151a758
PeshO2K <patienceotuke@gmail.com>
2023-02-23 02:41:18 -0800

    New alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,18 +5,17 @@
  * Write a function that prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 
  */
+void print_alphabet(void)
+{
+	int a = 97;
+	while(a <=122)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-void print_alphabet(void)
-{
-        int a = 97;
-
-        while(a <= 122)
-        {
-                putchar(a);
-        }
-        putchar('\n');
 }
