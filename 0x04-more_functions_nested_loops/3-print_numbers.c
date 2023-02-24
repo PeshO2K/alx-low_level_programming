cfcbbca6ab54ee130206eead8b087bba2f64c3f2
PeshO2K <patienceotuke@gmail.com>
2023-02-23 16:25:09 -0800

    betty corrections

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints numbers 0 - 9 
+ * print_numbers - prints numbers 0 - 9
  *
  * Return: nothing
  */
 void print_numbers(void)
 {
 	int n;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		_putchar(n);
