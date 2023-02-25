5bf890762a16753408b0106e34df9018cd4831d9
PeshO2K <patienceotuke@gmail.com>
2023-02-24 22:34:56 -0800

    Printing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - unix operating system
+ *
+ * Return: 1 succcess
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	return (1);
+}
