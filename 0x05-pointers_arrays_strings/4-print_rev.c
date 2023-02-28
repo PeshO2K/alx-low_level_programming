544014de3cab89d7e26252d248169cf6fc745bdf
PeshO2K <patienceotuke@gmail.com>
2023-02-28 06:15:53 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse followed by new line
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len, count;
+
+	len = _strlen(s)
+	while (len >= 0)
+	{
+		putchar(str[len]);
+		len--;
+	}
+	putchar('\n');
+
+
+}
