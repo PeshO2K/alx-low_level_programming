0509e333f0d4d9f55abc47a5ef98efe34185b86a
PeshO2K <patienceotuke@gmail.com>
2023-02-28 05:36:26 -0800

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return length of string
+ * @s: string pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return counter;
+}
