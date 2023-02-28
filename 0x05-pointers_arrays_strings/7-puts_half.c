07a457cc44f78ab9d64e934ce0f4d60fda856b0a
PeshO2K <patienceotuke@gmail.com>
2023-02-28 07:45:25 -0800

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints asecond half of string then new line
+ * @str: string to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0, i = 0, j;
+
+	while (str[i++])
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		j = len / 2;
+	}
+	else
+	{
+		j = (len + 1) / 2;
+	}
+	for (i = j; i < len; i ++)
+	{
+		putchar(str[i]);
+	}
+	putchar ('\n');
+}
