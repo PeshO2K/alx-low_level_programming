35b8781a0fac3cb4acbb1782851e4f0ef620799a
PeshO2K <patienceotuke@gmail.com>
2023-02-28 05:21:51 -0800

    Swapping horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps value of parameters passed in
+ * @a: pointer to first integer
+ * @b: pointer to second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int ap = *a;
+	*a = *b;
+	*b = *ap;
+
+}
