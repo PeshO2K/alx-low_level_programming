9fae814d68f4ae01030aaf847fe64a1ae645ff9b
PeshO2K <patienceotuke@gmail.com>
2023-07-06 16:50:00 -0700

    betty error fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,19 +14,4 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 	}
 	_putchar((n & 1) + '0');
-	/* alternate solution:buggy
-	*int i = 0, binary[64];
-*	while (n > 0)
-*	{
-*		binary[i] = (n & 1);
-*		n >>= 1;
-*		i++;
-*	}
-*	i--;
-*	while (i >= 0)
-*	{
-*		putchar(binary[i] + '0');
-*		i--;
-*	}
-	*/
 }
