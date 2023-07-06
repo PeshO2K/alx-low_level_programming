e0bee7d8b779e09700068680e08f8124cd5d02ba
PeshO2K <patienceotuke@gmail.com>
2023-07-06 16:55:39 -0700

    Betty error fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+ * get_bit - return value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-		
+
 	if (index < 63)
 	{
 		return ((n >> index) & 1);
