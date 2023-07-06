7f08f1ee0b2a9a86814dbf344faa19da72d1c06f
PeshO2K <patienceotuke@gmail.com>
2023-07-06 16:48:44 -0700

    removed printf

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
+/**
+ * print_binary - prints binary equivalent
+ * of a number
+ * @n: unsigned long int
+ *
+ * Return: nothing
+ */
 
 void print_binary(unsigned long int n)
 {
-	printf("n = %lu\n",n);
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-	printf(" %lu\n", (n&1));
-	
-	putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 	/* alternate solution:buggy
 	*int i = 0, binary[64];
 *	while (n > 0)
