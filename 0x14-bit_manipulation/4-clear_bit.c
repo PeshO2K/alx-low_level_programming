72c8acd9afc65c6c82ac10971333256f38632e5d
PeshO2K <patienceotuke@gmail.com>
2023-07-06 17:05:56 -0700

    betty error fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * clear_bit - sets the value of a bit to 0
+ * at a given index
+ * @n: pointer of an unsigned long int
+ * @index: index of the bit
+ *
+ * Return: 1 success, -1 error
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n != NULL && (index < (sizeof(unsigned long int) * 8) - 1))
