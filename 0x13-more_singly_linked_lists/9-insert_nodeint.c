defa091717a53112d9598a88d30375f0f77a2a76
PeshO2K <patienceotuke@gmail.com>
2023-07-23 07:30:42 -0700

    Out of bound check, adjusted limits

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -55,7 +55,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head)
 	{
 		cursor = *head;
-		if (idx >= listint_len(cursor))
+		if (idx > listint_len(cursor))
 		{
 				return (NULL);
 		}
