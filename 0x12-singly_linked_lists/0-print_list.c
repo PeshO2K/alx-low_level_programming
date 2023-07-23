dd8c0eb8d3c44f4164d66cb310a5aada6765a7b5
PeshO2K <patienceotuke@gmail.com>
2023-07-23 05:33:37 -0700

    New code to print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,6 +6,7 @@
  *
  * Return: the number of nodes
  */
+/*
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
@@ -15,6 +16,29 @@ size_t print_list(const list_t *h)
 		printf("[%u] ", (h->str == NULL) ? 0 : h->len);
 		printf("%s\n", (h->str == NULL) ? "(nil)" : h->str);
 		h = h->next;
+		n++;
+	}
+	return (n);
+}
+*/
+#include "lists.h"
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: pointer to list.
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int n = 0;
+	const list_t *tmp_node = h;
+
+	while (tmp_node)
+	{
+		if (tmp_node->str)
+			printf("[%u] %s\n", tmp_node->len, tmp_node->str);
+		else
+			printf("[0] (nil)\n");
+		tmp_node = tmp_node->next;
 		n++;
 	}
 	return (n);
