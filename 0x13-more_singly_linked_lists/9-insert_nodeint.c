83f49791949663cbc95c385a602fbd9c648b20f1
PeshO2K <patienceotuke@gmail.com>
2023-07-23 07:27:51 -0700

    Out of bound check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -52,10 +52,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	if (*head && idx < listint_len(*head))
+	if (*head)
 	{
 		cursor = *head;
-		
+		if (idx >= listint_len(cursor))
+		{
+				return (NULL);
+		}
 		count = 0;
 
 		while (count < idx - 1 && cursor)
