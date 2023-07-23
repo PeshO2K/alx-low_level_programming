6df2cfc4aa5ed629f0c7db06575f106fc0e5d695
PeshO2K <patienceotuke@gmail.com>
2023-07-23 06:03:45 -0700

    betty error fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -18,18 +18,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (count);
 }
-/*
-size_t print_listint(const listint_t *h)
-{
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-
-		count += 1;
-		h = h->next;
-	}
-	free(h);
-	return (count);
-}*/
