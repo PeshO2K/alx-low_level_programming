81ed0bc719d2259cc1d6e7f389fcff6fc30b4673
PeshO2K <patienceotuke@gmail.com>
2023-07-23 04:25:58 -0700

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,16 @@
  */
 void free_list(list_t *head)
 {
-	list_t *updated;
+/*	list_t *updated;*/
 
 	while (head)
 	{
-		updated = head;
+		free(head->str);
+		free(head);
+		head = head->next;
+		/*updated = head;
 		head = head->next;
 		free(updated->str);
-		free(updated)
+		free(updated)*/
 	}
 }
