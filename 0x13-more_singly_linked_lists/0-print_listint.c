9ccdd1b3c7c5d098f45526213a5335ca0928dfde
PeshO2K <patienceotuke@gmail.com>
2023-07-23 02:58:55 -0700

    New Approach to print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
  *
  * Return: the number of nodes
  */
+/*
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
@@ -13,6 +14,26 @@ size_t print_listint(const listint_t *h)
 	{
 		count++;
 		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}*/
+#include "lists.h" /* other header files included in lists.h*/
+
+/**
+ * print_listint - prints all elements of a list
+ * @h: list
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		count += 1;
 		h = h->next;
 	}
 	return (count);
