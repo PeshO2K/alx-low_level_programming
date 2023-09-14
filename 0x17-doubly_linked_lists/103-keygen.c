d76b54dded463535acd0b5fe6d7855ff706c5b12
Patience Otuke <patienceotuke@gmail.com>
2023-09-14 14:17:05 +0300

    Betty

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, c;
 	size_t len, tot;
-	char *alph = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *al = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char password[7] = "      ";
 
 	if (argc != 2)
@@ -22,24 +22,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	len = strlen(argv[1]);
-	password[0] = alph[(len ^ 59) & 63];
+	password[0] = al[(len ^ 59) & 63];
 	for (i = 0, tot = 0; i < len; i++)
 		tot += argv[1][i];
-	password[1] = alph[(tot ^ 79) & 63];
+	password[1] = al[(tot ^ 79) & 63];
 	for (i = 0, c = 1; i < len; i++)
 		c *= argv[1][i];
-	password[2] = alph[(c ^ 85) & 63];
+	password[2] = al[(c ^ 85) & 63];
 	for (c = argv[1][0], i = 0; i < len; i++)
 		if ((char)c <= argv[1][i])
 			c = argv[1][i];
 	srand(c ^ 14);
-	password[3] = alph[rand() & 63];
+	password[3] = al[rand() & 63];
 	for (c = 0, i = 0; i < len; i++)
 		c += argv[1][i] * argv[1][i];
-	password[4] = alph[(c ^ 239) & 63];
+	password[4] = al[(c ^ 239) & 63];
 	for (c = 0, i = 0; (char)i < argv[1][0]; i++)
 		c = rand();
-	password[5] = alph[(c ^ 229) & 63];
+	password[5] = al[(c ^ 229) & 63];
 	printf("%s\n", password);
 	return (0);
 }
