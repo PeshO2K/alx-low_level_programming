544f7734307fcdcd8be479cbd94a193d270dadef
Patience Otuke <patienceotuke@gmail.com>
2023-09-14 12:51:13 +0300

    betty 2

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,39 +7,39 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *curr;
-    size_t len;
+	dlistint_t *curr;
+	size_t len;
 
-    if (head && *head)
-    {
-        curr = *head;
-        for(len = 0; curr; len++, curr = curr->next)
-        {
-            ;
-        }
-        if (((index + 1) <= len))
-        {
-            curr = *head;
-            for (len = 0; (len < index) && curr; len++, curr = curr->next)
-            {
-                ;
-            }
-            if(curr->prev)
-            {
-                curr->prev->next = curr->next;
-            }
-            else
-            {
-                *head = curr->next;
-            }
-            
-            if (curr->next)
-            {
-                curr->next->prev = curr->prev;
-            }
-            free(curr);
-            return (1);
-        }
-    }
-    return (-1);
+	if (head && *head)
+	{
+		curr = *head;
+		for(len = 0; curr; len++, curr = curr->next)
+		{
+			;
+		}
+		if (((index + 1) <= len))
+		{
+			curr = *head;
+			for (len = 0; (len < index) && curr; len++, curr = curr->next)
+			{
+				;
+			}
+			if(curr->prev)
+			{
+				curr->prev->next = curr->next;
+			}
+			else
+			{
+				*head = curr->next;
+			}
+			
+			if (curr->next)
+			{
+				curr->next->prev = curr->prev;
+			}
+			free(curr);
+			return (1);
+		}
+	}
+	return (-1);
 }
