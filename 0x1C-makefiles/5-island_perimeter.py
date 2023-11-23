a5929cca967a8fff3a2e91f92c7df813bf5e9f7d
Patience Otuke <patienceotuke@gmail.com>
2023-11-23 05:31:11 +0300

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,15 +8,13 @@ the island described in grid:
 def island_perimeter(grid):
     """The function
     """
-    baseline = []
-    for i, row in enumerate(grid):
-        if i in range(1, len(grid)-1):
-            for j, cell in enumerate(row):
-                # if i == 1 and cell == 1:
-                #    baseline.append(j)
-
-                if ((i == 1) or j in baseline or
-                        (j-1 in baseline and row[j-1])) and cell == 1:
-                    baseline.append(j)
-
-    return (2 * len(baseline) + 2)
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
